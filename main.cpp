f7a69b87dfa8f07945c116815b37802aea2a1dd3
terryking0711 <terry9487turry0711@gmail.com>
2026-01-05 14:17:08 +0800

    update cpp file

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This project uses the C++17 language standard version." << std::endl;
+    return 0;
+}
